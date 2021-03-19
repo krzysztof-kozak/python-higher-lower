bc73f3a947b312fb934c9c04dfe3f3c1904e9caa
Kris <hithard5@gmail.com>
2021-03-19 15:53:51 +0000

    added some minor changes to string formatting

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def get_random_entry(data, label):
 
     entry = data[index]
     follower_count = entry["follower_count"]
-    return f"Compare {label}: {entry['name']}, a {entry['description']}, from {entry['country']}.", index, follower_count
+    return f"{label}: {entry['name']}, a {entry['description']}, from {entry['country']}.", index, follower_count
 
 
 entry_a, index_a, follower_count_a = get_random_entry(data, "A")
@@ -23,19 +23,21 @@ while not is_unique:
         is_unique = True
     else:
         entry_b, index_b, follower_count_b = get_random_entry(data, "B")
-        
+
 score = 0
 next_round = True
 while next_round:
     clear()
     print(logo)
+    if score > 0:
+        print(f"You are right! Current score: {score}\n")
 
-    print(entry_a)
+    print(f"Compare {entry_a} {follower_count_a}")
     print(vs)
-    print(entry_b)
+    print(f"Against {entry_b} {follower_count_b}")
 
 
-    answer = input("Who has more followers? Type a or b: ").lower()
+    answer = input("\nWho has more followers? Type a or b: ").lower()
 
     is_valid_answer = False
     while not is_valid_answer:
@@ -69,18 +71,6 @@ while next_round:
     other_option = answer_dict[comparison]["followers"]
 
     if player_guess > other_option:
-        #####   debug   #####
-        print(follower_count_a, follower_count_b)
-        print(f"You answered {answer} = {answer_dict[answer]['followers']}.")
-        print(f"score: {score}")
-
-        print("\n******\n")
-
-        print(f"NEW ENTRY A: {entry_a}")
-        print(f"NEW ENTRY B: {entry_b}")
-        ###############################
-
-
         print("Correct! Score +1")
         score += 1
 
@@ -92,7 +82,7 @@ while next_round:
 
     else:
         next_round = False
-        print(f"WRONG! Final score: {score}")
+        print(f"Wrong! Final score was: {score}")
 
 
 
